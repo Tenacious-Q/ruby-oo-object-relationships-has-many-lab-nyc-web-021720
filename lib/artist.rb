d6cfcaf13759102891d9a2b9d0c99b3c1efac1ec
Michael Quigley <mikequigley@quigleypublishing.com>
2020-03-01 06:18:54 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,6 +15,11 @@ class Artist
     song.artist = self
   end
 
+  def add_song_by_name(name)
+     song = Song.new(name)
+     song.artist = self
+  end
+
   def songs
     Song.all.select {|song| song.artist == self}
   end
